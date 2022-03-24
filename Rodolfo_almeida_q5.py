630319a95c9afe00bd193f336a8bbda692e06d38
rdfalmeida <82606681+rdfalmeida@users.noreply.github.com>
2022-03-24 04:36:44 -0300

    Delete Rodolfo_almeida_q5.py

--- Rodolfo_almeida_q5.py
+++
@@ -1,9 +0,0 @@
-# Exercício 6
-# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
-
-import math
-
-raio_circulo = float(input("Informe o raio do círculo: "))
-area_circulo = math.pi*raio_circulo**2
-
-print("A área do círculo é:", round(area_circulo, 4))
